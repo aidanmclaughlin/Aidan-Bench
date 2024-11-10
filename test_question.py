3b495293c3c52903bce50a09adf5698afff629e7
aidanmclaughlin <aidanmcl@hotmail.com>
2024-11-10 18:00:36 -0500

    test judges

--- test_question.py
+++ test_question.py
@@ -7,5 +7,8 @@ answers = []
 for _ in range(10):
     answer = gen_answer(question_to_test, answers, model_name="openai/gpt-4o")
     answers.append(answer)
+    judge_answer(question_to_test, answer, model_name="openai/gpt-4o")
+    judge_similarity(question_to_test, answer, answers[0], model_name="openai/gpt-4o")
+
     print("="*30)
     print(answer)
