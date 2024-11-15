ae5ee55641a5004557bf68c67c52000bb95f5917
aidanmclaughlin <aidanmcl@hotmail.com>
2024-11-15 14:36:48 -0500

    update models with azure info

--- models.py
+++ models.py
@@ -9,20 +9,20 @@ if os.getenv("AZURE_API_KEY"):
 else:
     print("Not using Azure API Key")
 
-@retry(tries=3)
+
+@retry(backoff=2)
 def chat_with_model(prompt, model, max_tokens=4000, temperature=0):
-    if "o1" in model.lower() and os.getenv("AZURE_API_KEY"):
+    if "o1" in model.lower() or "gpt-4o-2024-08-06" in model.lower() or "gpt-4-turbo" in model.lower() and os.getenv("AZURE_API_KEY"):
         model = model.replace("openai/", "")
         return azure.message_chat(
             [{"role": "user", "content": prompt}],
             model
         )
-        
     client = OpenAI(
         api_key=os.getenv("OPEN_ROUTER_KEY"),
         base_url="https://openrouter.ai/api/v1"
     )
-    
+
     extra_body = {}
     if model == "meta-llama/llama-3.1-405b-instruct:bf16":
         model = "meta-llama/llama-3.1-405b-instruct"
@@ -31,7 +31,7 @@ def chat_with_model(prompt, model, max_tokens=4000, temperature=0):
                 "quantizations": ["bf16"]
             }
         }
-    
+
     response = client.chat.completions.create(
         model=model,
         messages=[
