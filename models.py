5f34c5401287dd176691c863f6722dfcaafc474d
aidanmclaughlin <aidanmcl@hotmail.com>
2024-11-17 12:38:40 -0500

    remove azure

--- models.py
+++ models.py
@@ -2,22 +2,9 @@ from openai import OpenAI
 import os
 from functools import lru_cache
 from retry import retry
-import azure
-
-if os.getenv("AZURE_API_KEY"):
-    print("Using Azure API Key")
-else:
-    print("Not using Azure API Key")
-
 
 @retry(backoff=2)
-def chat_with_model(prompt, model, max_tokens=4000, temperature=0):
-    if "o1" in model.lower() or "gpt-4o-2024-08-06" in model.lower() or "gpt-4-turbo" in model.lower() and os.getenv("AZURE_API_KEY"):
-        model = model.replace("openai/", "")
-        return azure.message_chat(
-            [{"role": "user", "content": prompt}],
-            model
-        )
+def chat_with_model(prompt: str, model: str, max_tokens: int = 4000, temperature: float = 0) -> str:
     client = OpenAI(
         api_key=os.getenv("OPEN_ROUTER_KEY"),
         base_url="https://openrouter.ai/api/v1"
@@ -34,12 +21,7 @@ def chat_with_model(prompt, model, max_tokens=4000, temperature=0):
 
     response = client.chat.completions.create(
         model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
+        messages=[{"role": "user", "content": prompt}],
         max_tokens=max_tokens,
         temperature=temperature,
         extra_body=extra_body
@@ -49,9 +31,8 @@ def chat_with_model(prompt, model, max_tokens=4000, temperature=0):
 
 @lru_cache(maxsize=10000)
 @retry(tries=3)
-def embed(text):
+def embed(text: str) -> list[float]:
     client = OpenAI()
-
     response = client.embeddings.create(
         model="text-embedding-3-large", input=[text])
     return response.data[0].embedding
